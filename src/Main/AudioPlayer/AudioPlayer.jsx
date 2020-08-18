84987cc6e61e81762b0d841502287e0f2f64fa18
Denis <suboch.denny@gmail.com>
2020-08-18 16:59:53 +0300

    feat: add ref on player element

--- src/Main/AudioPlayer/AudioPlayer.jsx
+++ src/Main/AudioPlayer/AudioPlayer.jsx
@@ -4,10 +4,12 @@ import AudioPlayer from 'react-h5-audio-player'
 import './AudioPlayer.scss'
 
  function CustomAudioPlayer(props) {
-     const {audioSrc} = props;
+     // eslint-disable-next-line react/prop-types
+     const {audioSrc, playerEl} = props;
     return (
         <AudioPlayer
             src={audioSrc}
+            ref={playerEl}
             onPlay={(e) => console.log(e)}
             layout='horizontal-reverse'
             // other props here
