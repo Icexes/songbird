d467be24533192cb5625c6267ac86785627219c7
Denis <suboch.denny@gmail.com>
2020-08-18 16:59:20 +0300

    feat: add default description section

--- src/Main/Description/Description.jsx
+++ src/Main/Description/Description.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 import AudioPlayer from '../AudioPlayer/AudioPlayer'
 import './Description.scss';
 
 function Description(props) {
-    const { clickedMovie } = props;
-    console.log(clickedMovie);
+    const {clickedMovie} = props;
     if (clickedMovie) {
         return (
             <div className='movie-description'>
@@ -20,7 +20,25 @@ function Description(props) {
             </div>
         )
     }
-    return 1;
+    return (
+        <div className='movie-description'>
+            <div className='movie-description__rules'>
+                Прослушайте открывок из фильма.<br/>
+                Выберете подходящий фильм из предложенных вариантов.     
+            </div>
+        </div>
+    )
 }
-
+Description.propTypes = {
+    clickedMovie: PropTypes.shape({
+        posterSrc : PropTypes.string,
+        name: PropTypes.string,
+        translate: PropTypes.string,
+        audioSrc: PropTypes.string,
+        description: PropTypes.string
+    })
+}
+Description.defaultProps = {
+    clickedMovie: undefined
+}
 export default Description;
