2b9ad67908cb37423a8e51fb2f3a4579b8ac515b
Denis <suboch.denny@gmail.com>
2020-08-28 12:42:25 +0300

    feat: add proptypes

--- src/components/Main/Answer/AnswerElement/AnswerElement.jsx
+++ src/components/Main/Answer/AnswerElement/AnswerElement.jsx
@@ -1,10 +1,8 @@
-/* eslint-disable no-nested-ternary */
-/* eslint-disable no-debugger */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-/* eslint-disable react/forbid-prop-types */
-/* eslint-disable react/require-default-props */
+
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+import classes from 'classnames'
 
 function AnswerElement(props) {
     const { movie, index, handleMovieClick, isAnswered, isCorrect, setRoundScore } = props;
@@ -20,20 +18,16 @@ function AnswerElement(props) {
                     if (!isCorrect) {
                         newIndicatorState = 'red';
                         setRoundScore((prevScore) => prevScore - 1)
-
                     }
                     else {
                         newIndicatorState = 'green'
                     }
                 }
-
                 setindicatorState(newIndicatorState);
             }
 
-
-
         }}>
-            <span className={`movie-list__indicator ${indicatorState === 'red' ? 'movie-list__indicator--incorrect' : indicatorState === 'green' ? 'movie-list__indicator--correct' : ''}`} />
+            <span className={classes('movie-list__indicator', { 'movie-list__indicator--incorrect': indicatorState === 'red' }, { 'movie-list__indicator--correct': indicatorState === 'green' })} />
             <span className='movie-list__film-name'>{movie.name}</span>
         </li>
     )
@@ -41,11 +35,16 @@ function AnswerElement(props) {
 
 AnswerElement.propTypes = {
     handleMovieClick: PropTypes.func.isRequired,
-    movie: PropTypes.object,
-    index: PropTypes.number,
-    setRoundScore: PropTypes.func,
-    isAnswered: PropTypes.bool,
+    movie: PropTypes.shape({
+        name: PropTypes.string
+    }),
+    index: PropTypes.number.isRequired,
+    setRoundScore: PropTypes.func.isRequired,
+    isAnswered: PropTypes.bool.isRequired,
     isCorrect: PropTypes.bool.isRequired
 }
 
+AnswerElement.defaultProps = {
+    movie: {}
+}
 export default AnswerElement;
